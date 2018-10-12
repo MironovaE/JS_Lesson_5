df74b97f9badff6443a4ecff99396c3643e64107
MironovaE <Mironova.E.Pavl@sberbank.ru>
2018-10-12 16:12:16 +0300

    1.2

--- lesson_5.1.js
+++ lesson_5.1.js
@@ -1,9 +1,108 @@
-3. Написать функцию addZerosTo(num, strLength), принимающую два числа.
-*  Функция должна добавлять необходимое количество нулей к целому
-*  числу num, причем итоговая длина строки должна составлять
-*  strLength символов. Например,
-*  alert(addZerosTo(5, 3)); // '005'
-*  alert(addZerosTo(14, 3)); // '014'
-*  alert(addZerosTo(100, 3)); // '100'
-*  alert(addZerosTo(1234, 3)); // '1234'
-*ratePasswordOr())
+/**
+* 4. Написать функцию isInstanceOfUser(obj), принимающую что угодно,
+*  возвращающую true/false. true возвращать в случае, если в качестве аргумента
+*  передан объект, у которого есть свойства name и surname со строковым (!)
+*  значением и свойство age с числовым (!) значением. false возвращать
+*  во всех остальных случаях. Например,
+*  var vasya = {
+*        name: 'Вася',
+*        surname: 'Пупкин',
+*        age: 15
+*      };
+*
+*  var masha = {
+*        age: 17.5,
+*        name: 'Маша',
+*        surname: 'Пупкина'
+*      };
+*
+*  var petya = {
+*        name: 'Петя',
+*        dateOfBirth: '21.12.2012'
+*      };
+*
+*  alert(isInstanceOfUser(vasya)); // true
+*  alert(isInstanceOfUser(masha)); // true
+*  alert(isInstanceOfUser(petya)); // false
+*  alert(isInstanceOfUser(17)); // false
+*  alert(isInstanceOfUser(false)); // false
+*  alert(isInstanceOfUser('trololo')); // false
+*  alert(isInstanceOfUser({})); // false
+*/
+
+
+// Метод «arr.some(callback[, thisArg])» возвращает true, если вызов callback вернёт true для какого-нибудь элемента arr.
+//
+//     var arr = [1, -1, 2, -2, 3];
+//
+// function isPositive(number) {
+//     return number > 0;
+// }
+//
+
+// alert( arr.some(isPositive) ); // true, есть хоть одно положительное
+
+//
+// function isInstanceOfUser(obj) {
+//     var keys = Object.keys(obj);
+//     var arr = [];
+//    arr.push(keys.some(containsProperties,properties));
+// }
+//
+// function containsProperties(keys , properties) {
+//
+//      return (keys ===  properties);
+//  }
+//
+//  //console.log(isInstanceOfUser())
+//
+//
+//
+// var vasya = {
+//     age: 15,
+//     name: 'Вася',
+//     surname: 'Пупкин',
+//
+// };
+//
+// isInstanceOfUser(vasya);
+
+function isInstanceOfUser(obj) {
+    var keys = Object.keys(obj);
+    let arr = [];
+    for (let key in obj) {
+        if (key === 'age' && typeof(obj[key]) === "number") {
+            arr.push(true);
+            //continue;
+        }
+        else if ((key === 'name' || key === 'surname') && typeof(obj[key]) === "string") {
+            arr.push(true);
+           // continue;
+        }
+        // if (key === 'surname' && typeof(obj[key]) === "string") {
+        //     arr.push(true);
+        //     continue;
+        // }
+        else {
+            arr.push(false);
+           // continue;
+        }
+
+    }
+    var r = arr.every(containsProperties);
+    return r;
+}
+
+
+function containsProperties(arr) {
+
+     return arr === true;
+ }
+
+var petya = {
+           name: 'Петя',
+           dateOfBirth: '21.12.2012'
+         };
+
+var t = isInstanceOfUser(petya);
+
--- lesson_5.js
+++ lesson_5.js
@@ -97,6 +97,75 @@ alert("generatePasswordOr = " + generatePasswordOr());
  */
 
 let addZerosTo = (num, strLength) => {
-    let numLength = num.length;
-    
-}); // '1234'
+    let numStr = num.toString();
+    let arrNum = [];
+    if(numStr.length < strLength){
+        for(let i = 0; i < (strLength - numStr.length); i++){
+            arrNum.unshift(0);
+        }
+        arrNum.push(numStr);
+        //console.log(arrNum.join(''))
+        alert(arrNum.join(''));
+    }
+    else {
+        //console.log(num)
+         alert(num);
+    }
+}
+
+//or
+
+let addZerosTo = (num, strLength) => {
+    let numStr = num.toString();
+    let newStr = "";
+    if(numStr.length < strLength){
+        for(let i = 0; i < (strLength - numStr.length); i++){
+            newStr += 0;
+        }
+        newStr +=numStr;
+        //console.log(newStr)
+        alert(newStr);
+    }
+    else {
+        //console.log(numStr)
+        alert(newStr);
+    }
+}
+
+addZerosTo(5, 3); // '005'
+addZerosTo(14, 3); // '014'
+addZerosTo(100, 3); // '100'
+addZerosTo(1234, 3); // '1234'
+
+
+/**
+ * 4. Написать функцию isInstanceOfUser(obj), принимающую что угодно,
+ *  возвращающую true/false. true возвращать в случае, если в качестве аргумента
+ *  передан объект, у которого есть свойства name и surname со строковым (!)
+ *  значением и свойство age с числовым (!) значением. false возвращать
+ *  во всех остальных случаях. Например,
+ *  var vasya = {
+ *    name: 'Вася',
+ *    surname: 'Пупкин',
+ *    age: 15
+ *  };
+ *
+ *  var masha = {
+ *    age: 17.5,
+ *    name: 'Маша',
+ *    surname: 'Пупкина'
+ *  };
+ *
+ *  var petya = {
+ *    name: 'Петя',
+ *    dateOfBirth: '21.12.2012'
+ *  };
+ *
+ *  alert(isInstanceOfUser(vasya)); // true
+ *  alert(isInstanceOfUser(masha)); // true
+ *  alert(isInstanceOfUser(petya)); // false
+ *  alert(isInstanceOfUser(17)); // false
+ *  alert(isInstanceOfUser(false)); // false
+ *  alert(isInstanceOfUser('trololo')); // false
+ *  alert(isInstanceOfUser({})); // false
+ */
